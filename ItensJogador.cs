572d919cff6763fbbb153c62fe21325bdcaa5ce8
Vinicius <viniciusarielarruda@usp.br>
2019-01-12 16:30:18 -0200

    Agora o Jogador eh capaz de pegar itens

---
+++ ItensJogador.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItensJogador : MonoBehaviour {
+    //O Jogador deve ser capaz de ter dois tipos de itens:
+
+    //Itens de Mão:
+    //São Itens que não ficam dentro do inventário e têm uso imediato (plutônio para os puzzles, por exemplo)
+    //O Jogador pode carregar apenas UM item de Mão por vez
+
+    //Itens de Inventário
+    //São os itens que ficam dentro do inventário enquanto o Jogador não decidir descartá-los (armas, upgrades, ferramentas, consumíveis)
+    //A quantidade de itens de inventário que o Jogador pode carregar consigo é limitada pelo número de Espaços no Inventário
+
+
+    public bool CarregandoItemMao; //Verifica se o Jogador está carregando um item de mão no momento
+
+    public GameObject ItemMao;
+
+    public GameObject Jogador;
+	// Use this for initialization
+	void Start () {
+        CarregandoItemMao = false; //O Jogador nunca começa a fase carregando um item de Mão
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Largar Item de Mão
+        if(Input.GetKeyDown("e") && CarregandoItemMao)
+        {
+            CarregandoItemMao = false;
+
+            ItemMao.transform.position = Jogador.transform.position;
+
+            ItemMao.SetActive(true);
+
+            ItemMao.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+        }
+		
+	}
+}
---
+++ PegarPlutonio.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PegarPlutonio : MonoBehaviour {
+
+    bool PertoPlutonio; //Verifica se o Jogador está perto desse pedaço de plutônio
+
+    public GameObject Plutonio;
+
+    public GameObject Jogador;
+
+	// Use this for initialization
+	void Start () {
+        PertoPlutonio = false; //O Jogador começa longe do pedaço de plutônio
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PertoPlutonio && Input.GetKeyDown("e") && !Jogador.GetComponent<ItensJogador>().CarregandoItemMao)
+        {
+            Jogador.GetComponent<ItensJogador>().CarregandoItemMao = true;
+            Jogador.GetComponent<ItensJogador>().ItemMao = Plutonio;
+            Plutonio.SetActive(false);
+        }
+	}
+
+    //OBS: Por quê não usar OnTriggerStay?
+    //OnTriggerStay é executada a todo frame, enquanto o Jogador estiver em contato com o BoxCollider do Plutônio, devido a isso o códico não é executado como esperado
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Jogador"))
+        {
+            PertoPlutonio = true;
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Chao"))
+        {
+            Plutonio.GetComponent<Rigidbody2D>().gravityScale = 0;
+            Plutonio.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Jogador"))
+        {
+            PertoPlutonio = false;
+        }
+    }
+}
