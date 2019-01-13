f649d78f5da95c872547d37d9ea932e87350751e
Vinicius <viniciusarielarruda@usp.br>
2019-01-13 14:53:23 -0200

    Agora temos um background e um script para que ele siga a camera

---
+++ PanoDeFundoSegir.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanoDeFundoSegir : MonoBehaviour {
+
+    public GameObject Camera;
+
+    public GameObject PanoDeFundo;
+
+    float XCamera;
+
+    float YCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        XCamera = Camera.transform.position.x;
+
+        YCamera = Camera.transform.position.y;
+
+        PanoDeFundo.transform.position = new Vector3(XCamera, YCamera, PanoDeFundo.transform.position.z);
+	}
+}
